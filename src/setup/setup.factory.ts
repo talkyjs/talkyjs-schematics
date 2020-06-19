47833debb43e39a2c7db3b19c61e99e428d722e9
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-20 00:16:50 +0900

    feat: auto update tsconfig.json when using tsx

--- src/setup/setup.factory.ts
+++ src/setup/setup.factory.ts
@@ -1,15 +1,17 @@
 import {
-  Rule, chain, noop,
+  Rule, chain, noop, Tree, SchematicContext, SchematicsException,
 } from '@angular-devkit/schematics';
+import { TsConfigJson } from 'type-fest'
 import { addNodePackageDependenciesTask } from '../share/packages.factory';
 
 export function main({
-    ssml
+    ssml,
 }: {
-    ssml: 'default' | 'ssml'
+    ssml: 'default' | 'tsx'
 }): Rule {
   return () => {
     return chain([
+        updateTsConfig(ssml),
         addNodePackageDependenciesTask('dependencies',{
             "@ask-utils/router": "3.x",
         }),
@@ -21,4 +23,27 @@ export function main({
         })
     ])
   };
+}
+
+const updateTsConfig = (ssml: 'default' | 'tsx'): Rule => {
+    if (ssml !== 'tsx') return noop()
+    return (tree: Tree, _context: SchematicContext) => {
+      
+        const buf = tree.read('tsconfig.json');
+        if (!buf) {
+          throw new SchematicsException('cannot find package.json');
+        }
+        const content:TsConfigJson = JSON.parse(buf.toString('utf-8'));
+        if (content.compilerOptions) {
+            content.compilerOptions.jsx = "react"
+        } else {
+            content.compilerOptions = {
+                jsx: "react"
+            }
+        }
+
+        tree.overwrite('tsconfig.json', JSON.stringify(content, null, 2));
+
+        return tree;
+      };
 }
