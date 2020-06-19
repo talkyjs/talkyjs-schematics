6930ca8defa1d3f311da6f9cfa03db08351225a1
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-20 00:09:17 +0900

    feat: merge tsx to handler

--- src/handlers/files/__name@dasherize__.handler.ts
+++ src/handlers/files/__name@dasherize__.handler.ts
@@ -1,10 +1,20 @@
 import { Router } from "@ask-utils/router";
+<% if (ssml === 'tsx') { %>
+import { <%= classify(name)%>Script } from './<%= dasherize(name)%>.speech'
+<% } %>
 
 export const <%= classify(name)%>Handler: Router = {
     requestType: 'IntentRequest',
     intentName: "<%= classify(name)%>",
     handler: async (handlerInput) => {
+        <% if (ssml === 'tsx') { %>
+        const script = new <%= classify(name)%>Script(handlerInput)
+        return script
+            .createResponseBuilder()
+            .getResponse();
+        <% } else { %>
         return handlerInput.responseBuilder.getResponse()
+        <% } %>
     }
 }
 
